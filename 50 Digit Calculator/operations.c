2cb67e98aede37ca572ff0669c263394f277b8fd
Subhadeep <subhadeepdhang1@gmail.com>
2022-10-11 21:59:06 +0530

    updated dependencies

--- 50 Digit Calculator/operations.c
+++ 50 Digit Calculator/operations.c
@@ -5,5 +5,20 @@
 
 void operation_addition (char num1_arr[], char num2_arr[], char result_arr[])
 {
+
+}
+
+void operation_subtraction (char num1_arr[], char num2_arr[], char result_arr[])
+{
+    
+}
+
+void operation_multiplication (char num1_arr[], char num2_arr[], char result_arr[])
+{
+    
+}
+
+void operation_division (char num1_arr[], char num2_arr[], char result_arr[])
+{
     
 }
