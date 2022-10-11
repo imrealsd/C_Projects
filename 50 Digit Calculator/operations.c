3c7a706ade1267f03ea5863edb63c391ffbb1183
Subhadeep <subhadeepdhang1@gmail.com>
2022-10-11 21:29:51 +0530

    Project Calucator initiated

---
+++ 50 Digit Calculator/operations.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operations.h"
+
+
+void operation_addition (char num1_arr[], char num2_arr[], char result_arr[])
+{
+    
+}
---
+++ 50 Digit Calculator/operations.h
@@ -0,0 +1,12 @@
+#ifndef OPERATION_H
+#define OPERATION_H
+
+#define INPUT_SIZE 51                 // MAX 50 digit  umber
+#define ADDITION_OUTPUT_SIZE 52
+#define SUBTRACTION_OUTPUT_SIZE 51
+#define MULTIPLICATION_OUTPUT_SIZE 101
+#define DIVISION_OUTPUT_SIZE 51
+
+void operation_addition (char num1_arr[], char num2_arr[], char result_arr[]);
+
+#endif
