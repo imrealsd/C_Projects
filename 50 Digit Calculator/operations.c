9c70e3bc2882b682f7721efc07a9ae8397f67564
Subhadeep <subhadeepdhang1@gmail.com>
2022-11-03 13:25:47 +0530

    added addition_operation logic

--- 50 Digit Calculator/operations.c
+++ 50 Digit Calculator/operations.c
@@ -4,8 +4,17 @@
 
 
 void operation_addition (char num1_arr[], char num2_arr[], char result_arr[])
-{
-    
+{   
+    int sum = 0, remainder = 0, carry = 0, temp = 0;
+
+    for (int i = MAX_INPUT_SIZE-2; i >= 0; i--){
+
+        temp  = carry + num1_arr[i] + num2_arr[i] - (2*48);
+        sum   = temp % 10;
+        carry = temp / 10;
+        result_arr[i+1] = sum + 48;
+    }
+    result_arr[0] = carry + 48;
 }
 
 void operation_subtraction (char num1_arr[], char num2_arr[], char result_arr[])
--- 50 Digit Calculator/operations.h
+++ 50 Digit Calculator/operations.h
@@ -1,12 +1,15 @@
 #ifndef OPERATION_H
 #define OPERATION_H
 
-#define MAX_INPUT_SIZE 51                 // MAX 50 digit  umber
-#define ADDITION_OUTPUT_SIZE 52
-#define SUBTRACTION_OUTPUT_SIZE 51
-#define MULTIPLICATION_OUTPUT_SIZE 101
-#define DIVISION_OUTPUT_SIZE 51
+#define MAX_INPUT_SIZE 51                                          // MAX 50 digit  number
+#define ADDITION_OUTPUT_SIZE           (MAX_INPUT_SIZE + 1)
+#define SUBTRACTION_OUTPUT_SIZE        (MAX_INPUT_SIZE)
+#define MULTIPLICATION_OUTPUT_SIZE     ((2*MAX_INPUT_SIZE)-1)
+#define DIVISION_OUTPUT_SIZE           (MAX_INPUT_SIZE)
 
 void operation_addition (char num1_arr[], char num2_arr[], char result_arr[]);
+void operation_subtraction (char num1_arr[], char num2_arr[], char result_arr[]);
+void operation_multiplication (char num1_arr[], char num2_arr[], char result_arr[]);
+void operation_division (char num1_arr[], char num2_arr[], char result_arr[]);
 
 #endif
