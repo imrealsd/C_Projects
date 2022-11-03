7ba06870c850b8e77b6e50a98c83b61cf86087ea
Subhadeep <subhadeepdhang1@gmail.com>
2022-11-03 13:25:17 +0530

    modified main code

--- 50 Digit Calculator/main.c
+++ 50 Digit Calculator/main.c
@@ -4,16 +4,22 @@
 #include "operations.h"
 
 void choose_option(void);
-void copy_to_num_arr (char num[], char buff[], int8_t num_size);
+void copy_to_num_arr (char num[], char buff[], int num_size);
+
+char inpt_buff[MAX_INPUT_SIZE];
+char num1[MAX_INPUT_SIZE], num2[MAX_INPUT_SIZE];
+char add_result[ADDITION_OUTPUT_SIZE], sub_result[SUBTRACTION_OUTPUT_SIZE];
+char multi_result[MULTIPLICATION_OUTPUT_SIZE], div_result[DIVISION_OUTPUT_SIZE];
+
 
 int main(int argc, char *argv[])
 {   
-    int8_t cont = 1;
+    int cont = 1;
 
     while (cont){
         choose_option();
-        printf("Want to Continue ? press 'y' for yes , 'n' for no:");
-        scanf("%hhd", &cont);
+        printf("Want to Continue ? press '1' for yes , '0' for no:");
+        scanf("%d", &cont);
     }
     return 0;
 }
@@ -21,14 +27,13 @@ int main(int argc, char *argv[])
 
 void choose_option(void)
 {   
-    char inpt_buff[MAX_INPUT_SIZE];
-    char num1[MAX_INPUT_SIZE], num2[MAX_INPUT_SIZE];
-    int8_t opt;
+    int opt = 0;
 
     printf("Choose operation:\n");
     printf("1.Addition, 2.Subtraction, 3.Multiplication, 4.Division\n");
     printf("Enter your choice:");
-    scanf("%hhd", &opt);
+    scanf("%d", &opt);
+
 
     memset(inpt_buff,0,MAX_INPUT_SIZE);
     printf("input first number:");
@@ -43,16 +48,22 @@ void choose_option(void)
     switch (opt){
     
     case 1:
-        
+        memset(add_result,0,ADDITION_OUTPUT_SIZE);
+        operation_addition(num1,num2,add_result);
+        printf("Addition Result: %s\n", add_result);
+        break;
+    
+    case 2:
+
     }
 }
 
-void copy_to_num_arr (char num[], char buff[], int8_t num_size)
+void copy_to_num_arr (char num[], char buff[], int num_size)
 {   
     /*Getting the difference between max size and input size to get right shifted new start index */
 
-    int8_t input_size = strlen(buff);
-    int8_t new_start_index = num_size - input_size - 1;  // 1 for null character
+    int input_size = strlen(buff);
+    int new_start_index = num_size - input_size - 1;  // 1 for null character
 
     memset(num,0,MAX_INPUT_SIZE);
 
@@ -61,13 +72,11 @@ void copy_to_num_arr (char num[], char buff[], int8_t num_size)
      * step 2. copying the array from buffer [from index 0] to num [from new start index]
     */
 
-    for (int8_t i = 0; i < new_start_index; i++){
+    for (int i = 0; i < new_start_index; i++){
         num[i] = '0';
     }
-    for (int8_t i = 0; i < input_size; i++){
+    for (int i = 0; i < input_size; i++){
         num[new_start_index] = buff[i];
         new_start_index++;
     }
-
-    printf("%s\n", num);
 }
