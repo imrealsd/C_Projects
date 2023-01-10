41b7d169cb4bab411dcf4079a1beb736c538e35f
Subhadeep <subhadeepdhang1@gmail.com>
2023-01-10 17:57:06 +0530

    adjusted main logic

--- Calculator-Server/Server/main.c
+++ Calculator-Server/Server/main.c
@@ -34,13 +34,16 @@ int main(int argc, char *argv[])
 
     while(1){
 
-        printf("hello\n");
         waitFor_client();
 
         while (cont){
 
             get_input(num1, num2, &choice);
             initiate_operation(num1, num2, &choice);
+
+            if (client_alive() == 0){
+                break;
+            }
         }
         close_connection();
     }
@@ -61,7 +64,6 @@ static void initiate_operation(char *num1, char *num2, int *choice)
         /*reset result array, add numbers & display result*/
         memset(add_result,0,ADDITION_OUTPUT_SIZE);
         operation_addition(num1,num2,add_result);
-        display_result(add_result, ADDITION_OUTPUT_SIZE);
         send_output(add_result);
         break;
     
@@ -69,7 +71,6 @@ static void initiate_operation(char *num1, char *num2, int *choice)
         /*reset result array, subtract numbers & display result*/
         memset(sub_result,0,SUBTRACTION_OUTPUT_SIZE);
         operation_subtraction(num1,num2,sub_result, &negative_flag);
-        display_result(sub_result, SUBTRACTION_OUTPUT_SIZE);
         if (negative_flag == 1)
             send_output("-");
         else
@@ -81,7 +82,6 @@ static void initiate_operation(char *num1, char *num2, int *choice)
         /*reset result array, multiply numbers & display result*/
         memset(multi_result,'0',MULTIPLICATION_OUTPUT_SIZE);
         operation_multiplication(num1, num2, multi_result);
-        display_result(multi_result, MULTIPLICATION_OUTPUT_SIZE);
         send_output(multi_result);
         break;
 
@@ -90,10 +90,6 @@ static void initiate_operation(char *num1, char *num2, int *choice)
         memset(div_result,'0', DIVISION_OUTPUT_SIZE);
         memset(div_remainder, '0', DIVISION_OUTPUT_SIZE);
         operation_division(num1, num2, div_result, div_remainder);
-        printf("Quotient ");
-        display_result(div_result, DIVISION_OUTPUT_SIZE);
-        printf("Remainder ");
-        display_result(div_remainder, DIVISION_OUTPUT_SIZE);
         send_output(div_result);
         send_output(div_remainder);
         break;
@@ -102,27 +98,4 @@ static void initiate_operation(char *num1, char *num2, int *choice)
 }
 
 
-/**
- * @brief  Display result after calculation
- * @retval none
- */
-static void display_result(char *result, int size)
-{   
-    int first_num_encountered_flag = 0;
-    printf("Result: ");
 
-    /*print negative sign in case subtraction: [negative-flag = 1]*/
-    if (choice == 2 && negative_flag){printf("-");}
-    
-
-    /*print only after the first non zero element is found*/
-    for (int i = 0; i < size; i++){
-        
-        if (result[i] != '0')
-            first_num_encountered_flag = 1;
-
-        if (first_num_encountered_flag == 1)
-            printf("%c", result[i]);
-    }
-    printf("\n");
-}
