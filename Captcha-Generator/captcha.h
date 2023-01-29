47f4960710a05285e6b2c90b739821a9e2eb757d
Subhadeep <subhadeepdhang1@gmail.com>
2023-01-29 20:15:14 +0530

    added defines & func, prototypes

--- Captcha-Generator/captcha.h
+++ Captcha-Generator/captcha.h
@@ -5,4 +5,16 @@
 #ifndef CAPTCHA_H
 #define CAPTCHA_H
 
+#define TOTAL_NUM 10
+#define TOTAL_LETTER 26
+#define TOTAL_SPL_CHAR 20
+#define ARITHMATIC_CAP_SIZE 9           // ex. 99-99 = ?
+#define ALPHANUMERIC_CAP_SIZE 5         // 5 char 
+#define GEN_CAP_SIZE 10                 // 9 char + 1 null char
+#define TOTAL_OPERATION 2
+#define MIN_THREE_DIG_NUM 100
+
+void captcha_generateCaptcha(char* captcha);
+void captcha_displayCaptcha(const char* const captcha);
+
 #endif //  CAPTCHA_H
