b9531fb1a1156f47c8e01f193430100f5d4cfa2f
Subhadeep <subhadeepdhang1@gmail.com>
2023-01-30 21:32:50 +0530

    coded main logic

--- Captcha-Generator/main.c
+++ Captcha-Generator/main.c
@@ -6,5 +6,17 @@
 
 int main(int argc, char* argv[])
 {
-    
+    char systemCaptcha[GEN_CAP_SIZE];
+    char userInputCaptcha[GEN_CAP_SIZE];
+
+    captcha_generateCaptcha(systemCaptcha);
+    captcha_displayCaptcha(systemCaptcha);
+    captcha_takeUserInput(userInputCaptcha);
+
+    if (captcha_isCaptchaMatched(systemCaptcha, userInputCaptcha)){
+        printf("Captcha verified !!\n");
+    } else {
+        printf("\nWrong captcha !!\n");
+    }
+    return 0;
 }
