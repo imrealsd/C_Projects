b7ff65b1c227978645dd553cfed87d427603f4de
Subhadeep <subhadeepdhang1@gmail.com>
2023-01-31 17:07:36 +0530

    added comments

--- Captcha-Generator/main.c
+++ Captcha-Generator/main.c
@@ -1,14 +1,24 @@
+/*public includes*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/*private includes*/
 #include "captcha.h"
 
 
 int main(int argc, char* argv[])
-{
+{   
+    // Arrays for storing cpatchas
     char systemCaptcha[GEN_CAP_SIZE];
     char userInputCaptcha[GEN_CAP_SIZE];
 
+    /**
+     * Generate captcha
+     * Display captcha
+     * Take user input
+     * Comapre / match systemCaptcha with userInputCaptcha
+     */
     captcha_generateCaptcha(systemCaptcha);
     captcha_displayCaptcha(systemCaptcha);
     captcha_takeUserInput(userInputCaptcha);
