9bcb38f26c2f36dc0e3b3b87a4ec712e49a3c7c5
Subhadeep <subhadeepdhang1@gmail.com>
2023-01-31 17:07:19 +0530

    replaced 48 with ascii_offset macro

--- Captcha-Generator/captcha.c
+++ Captcha-Generator/captcha.c
@@ -1,10 +1,32 @@
 #include "captcha.h"
 #include <time.h>
 
-
+/**
+ * @brief check if a number is odd or even
+ * @param time_t(long)  num
+ * @retval int
+ */
 static int is_odd(time_t num);
+
+/**
+ * @brief swap two integers
+ * @param  int* num1, int* num2 
+ * @retval none 
+ */
 static void swap(int* num1, int* num2);
+
+/**
+ * @brief generate a alphanumerical captcha
+ * @param char* chaptcha 
+ * @retval none 
+ */
 static void captcha_generateAlphanumeric(char* captcha);
+
+/**
+ * @brief generate a arithmatic captcha
+ * @param char* chaptcha 
+ * @retval none 
+ */
 static void captcha_generateArithmatic(char* captcha);
 
 
@@ -29,10 +51,15 @@ void captcha_generateCaptcha(char* captcha)
     char alphaNumericCaptcha[ALPHANUMERIC_CAP_SIZE];
     time_t randNum = clock();
 
+    /*initialize all array elemets to zero*/
     memset(captcha, 0, GEN_CAP_SIZE);
     memset(arithmaticCaptcha, 0, ARITHMATIC_CAP_SIZE);
     memset(alphaNumericCaptcha, 0, ALPHANUMERIC_CAP_SIZE);
 
+    /**
+     * Generate alphanumeric captcha if randNumm is odd , else genearte arithmatic captcha
+     * copy generated captcha into user passed array (char* captcha)
+    */
     if (is_odd(randNum)){
         captcha_generateAlphanumeric(alphaNumericCaptcha);
         strncpy(captcha, alphaNumericCaptcha, ALPHANUMERIC_CAP_SIZE);
@@ -52,7 +79,15 @@ static int is_odd(time_t num)
 
 
 static void captcha_generateAlphanumeric(char* captcha)
-{
+{   
+    /**
+     * generate 5 randum numbers for 5 indexes
+     * 1 for capital letter
+     * 1 for small letter
+     * 2 for numbers
+     * 1 for special charcter
+     * put them into alphanumericCaptcha array
+     */
     time_t randNum = clock();
     captcha[0] = smallLetters[(randNum % TOTAL_LETTER)];
     randNum = clock();
@@ -75,6 +110,7 @@ static void captcha_generateArithmatic(char* captcha)
     int opIndex;
     int temp;
 
+    /*generate two (2-digit) numbers & operation symbol ('+','-)*/
     randNum = clock();
     num1 = (randNum % MIN_THREE_DIG_NUM);
     randNum = clock();
@@ -82,15 +118,20 @@ static void captcha_generateArithmatic(char* captcha)
     randNum = clock();
     num2 = ((3 *randNum) % MIN_THREE_DIG_NUM);
 
+    /**
+     * swap num1 & num2 if num1 > num2 to make sure num2 is always bigger than num1
+     * This is to avoid negative result i.e num2-num1 = always +ve
+     */
     if (num1 > num2){
        swap(&num1, &num2);
     }
     
-    captcha[1] = ((num2 / 10) % 10) + 48;
-    captcha[0] = (num2 % 10) + 48;
+    /*put [num1][operation symbol][num1][space]['='][space]['?'] into arithmaticCpatcha array*/
+    captcha[1] = ((num2 / 10) % 10) + ASCII_OFFSET_VAL;
+    captcha[0] = (num2 % 10) + ASCII_OFFSET_VAL;
     captcha[2] = operation[opIndex];
-    captcha[4] = ((num1 / 10) % 10) + 48;
-    captcha[3] = (num1 % 10) + 48;
+    captcha[4] = ((num1 / 10) % 10) + ASCII_OFFSET_VAL;
+    captcha[3] = (num1 % 10) + ASCII_OFFSET_VAL;
     captcha[5] = ' ';
     captcha[6] = '=';
     captcha[7] = ' ';
@@ -111,6 +152,7 @@ void captcha_displayCaptcha(const char* const captcha)
     char *alphaNumSpacePadding  =  "    " ;
     char *arithmaticSpacePadding = " " ;
 
+    /*print captcha insside a nice box*/
     printf("\n --Captcha--\n");
     printf("----------------\n");
     printf("|              |\n");
@@ -141,6 +183,7 @@ int captcha_isCaptchaMatched(char* systeCaptcha, char* userInputCaptcha)
     int usrResult = 0;
     int index = 0;
 
+    /*incase of alphanumeric captcha just compare the string & return 1 (match) or 0 (no match)*/
     if (strlen(systeCaptcha) == ALPHANUMERIC_CAP_SIZE){
 
         if (!strncmp(systeCaptcha, userInputCaptcha, ALPHANUMERIC_CAP_SIZE)){
@@ -148,22 +191,31 @@ int captcha_isCaptchaMatched(char* systeCaptcha, char* userInputCaptcha)
         }
         return 0;
 
+    /**
+     * incase of arithmatic captcha, extract num1,num2,operation symbol form system captcha
+     * extract user input (useResult) from userInputcaptcha
+     * calculate result of num1[operatiin][num2]
+     * compare result with userResult
+     * return 0 (wrong input) 1 (correct input)
+     */
     } else if (strlen(systeCaptcha) == ARITHMATIC_CAP_SIZE) {
 
-        num1 = ((systeCaptcha[0] - 48) * 10) + (systeCaptcha[1] - 48);
-        num2 = ((systeCaptcha[3] - 48) * 10) + (systeCaptcha[4] - 48);
+        num1 = ((systeCaptcha[0] - ASCII_OFFSET_VAL) * 10) + (systeCaptcha[1] - ASCII_OFFSET_VAL);
+        num2 = ((systeCaptcha[3] - ASCII_OFFSET_VAL) * 10) + (systeCaptcha[4] - ASCII_OFFSET_VAL);
 
+        // extarct user input result
         while (userInputCaptcha[index] != 0){
-            usrResult = (usrResult * 10) + (userInputCaptcha[index] - 48);
+            usrResult = (usrResult * 10) + (userInputCaptcha[index] - ASCII_OFFSET_VAL);
             index++;
         }
 
+        // extarct operation
         if (systeCaptcha[2] == '+'){
             result = num1 + num2;
         } else {
             result = num1 - num2;
         }
-
+        // comapre user & system result
         if (result == usrResult){
             return 1;
         }
@@ -172,8 +224,3 @@ int captcha_isCaptchaMatched(char* systeCaptcha, char* userInputCaptcha)
     return 0;
 }
 
-
-
-// TODO 1. Take typed captcha form user 
-//      2. match user typed captcha with system generated captcha
-//      3. write main() func 
