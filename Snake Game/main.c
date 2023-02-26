d989ff3f058cb7c4721c7094ae7fb57f068fff33
Subhadeep <subhadeepdhang1@gmail.com>
2023-02-26 12:45:01 +0530

    added rough main code

--- Snake Game/main.c
+++ Snake Game/main.c
@@ -3,6 +3,8 @@
 /*Global Variables*/
 char gameScreen[SCREEN_ROW][SCREEN_COL];
 snakeUnit *pSnakeHead;
+int foodRow;
+int foodCol;
 char userInput;
 
 
@@ -12,16 +14,29 @@ void changeTerminalBehaviour(void);
 
 int main(int argc, char* argv[])
 {    
+    pSnakeHead = (snakeUnit *)malloc(sizeof(snakeUnit));
     /*change terminal behaviour to take input without pressing enter*/
-    changeTerminalBehaviour();
+    //changeTerminalBehaviour();
     
     /*Initialise Game Screen*/
     snake_gameInit(gameScreen, pSnakeHead);
-    snake_displayGameScreen(gameScreen);
+    snake_generateFoodPosition(&foodRow, &foodCol);
+    snake_addFoodToGameScreen(gameScreen, foodRow, foodCol);
+    snake_displayGameScreen(pSnakeHead, gameScreen);
 
     while (True){
+
         userInput = getchar();
-        break;
+        pSnakeHead = snake_updateSnakePosition(userInput, pSnakeHead);
+
+        if (snake_isEatingFood(foodRow, foodCol, pSnakeHead)){
+            pSnakeHead = snake_increaseSnake(pSnakeHead, foodRow, foodCol);
+            snake_generateFoodPosition(&foodRow, &foodCol);
+        }
+        snake_resetGameScreen(gameScreen);
+        snake_addSnakeToGameScreen(pSnakeHead, gameScreen);
+        snake_addFoodToGameScreen(gameScreen, foodRow, foodCol);
+        snake_displayGameScreen(pSnakeHead, gameScreen);
     }
     return 0;
 }
