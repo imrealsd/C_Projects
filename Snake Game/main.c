eb82bd4c3e25a3ac72ef9daedea8f9293369d95d
Subhadeep <subhadeepdhang1@gmail.com>
2023-03-01 20:27:40 +0530

    added game termination func

--- Snake Game/main.c
+++ Snake Game/main.c
@@ -1,23 +1,23 @@
-#include "snake.h"
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
 
 /*Global Variables*/
 char gameScreen[SCREEN_ROW][SCREEN_COL];
 snakeUnit *pSnakeHead;
-int foodRow;
-int foodCol;
+int  gFoodRow;
+int  gFoodCol;
 char userInput[2];
-char userInputCopy;
-int userInputStatus;
-int snakeTailRow;
-int snakeTailCol;
+char gUserInputCopy;
+int  gUserInputStatus;
+int  gSnakeTailRow;
+int  gSnakeTailCol;
+int  gScore = 0;
 
 
 /*Function Prototypes*/
 void changeTerminalBehaviour(void);
 void resetTerminalBehaviour(void);
 void readUserInput(int timeout);
+void terminateGame(void);
 
 
 int main(int argc, char* argv[])
@@ -26,26 +26,27 @@ int main(int argc, char* argv[])
     
     /*Initialise Game Screen*/
     snake_gameInit(gameScreen, pSnakeHead);
-    snake_generateFoodPosition(&foodRow, &foodCol);
-    snake_addFoodToGameScreen(gameScreen, foodRow, foodCol);
-    snake_displayGameScreen(pSnakeHead, gameScreen);
+    snake_generateFoodPosition(&gFoodRow, &gFoodCol);
+    snake_addFoodToGameScreen(gameScreen, gFoodRow, gFoodCol);
+    snake_displayGameScreen(pSnakeHead, gameScreen, gScore);
 
     while (True){
 
-        changeTerminalBehaviour();
         readUserInput(1);
-        resetTerminalBehaviour();
+        if (userInput[0] == 'q')
+            terminateGame();
 
-        pSnakeHead = snake_updateSnakePosition(userInputCopy, pSnakeHead, &snakeTailRow, &snakeTailCol);
+        pSnakeHead = snake_updateSnakePosition(gUserInputCopy, pSnakeHead, &gSnakeTailRow, &gSnakeTailCol);
 
-        if (snake_isEatingFood(foodRow, foodCol, pSnakeHead)){
-            snake_increaseSnake(snakeTailRow, snakeTailCol, pSnakeHead);
-            snake_generateFoodPosition(&foodRow, &foodCol);
+        if (snake_isEatingFood(gFoodRow, gFoodCol, pSnakeHead)){
+            snake_increaseSnake(gSnakeTailRow, gSnakeTailCol, pSnakeHead);
+            snake_updateSocreBoard(&gScore);
+            snake_generateFoodPosition(&gFoodRow, &gFoodCol);
         }
         snake_resetGameScreen(gameScreen);
         snake_addSnakeToGameScreen(pSnakeHead, gameScreen);
-        snake_addFoodToGameScreen(gameScreen, foodRow, foodCol);
-        snake_displayGameScreen(pSnakeHead, gameScreen);
+        snake_addFoodToGameScreen(gameScreen, gFoodRow, gFoodCol);
+        snake_displayGameScreen(pSnakeHead, gameScreen, gScore);
     }
     return 0;
 }
@@ -53,16 +54,35 @@ int main(int argc, char* argv[])
 
 
 void readUserInput(int timeout)
-{
+{    
+    changeTerminalBehaviour();
+
+    /*setup for user input with a fixed timeout*/
     fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
     sleep(timeout);
-    userInputStatus = read(0, userInput, 2);
+    gUserInputStatus = read(0, userInput, 2);
 
-    if (userInputStatus != -1){
-            userInputCopy = userInput[0];
+    resetTerminalBehaviour();
+
+    /*if there's an input*/
+    if (gUserInputStatus != -1){
+        
+        /*take only valid inputs*/
+        if (userInput[0] == 'w' || userInput[0] == 's' || userInput[0] == 'a' || 
+                                    userInput[0] == 'd' || userInput[0] == 'q'){
+
+            gUserInputCopy = userInput[0];
+        } 
     } 
 }
 
+
+void terminateGame(void)
+{
+    system("clear");
+    printf("Game Closed\r\n");
+    exit(0);
+}
 
 void changeTerminalBehaviour(void)
 {   
