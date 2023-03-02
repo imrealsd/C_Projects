a9ff6d89b085f8c834b909352241dfd1085a5143
Subhadeep <subhadeepdhang1@gmail.com>
2023-03-02 13:13:16 +0530

    added comments

--- Snake Game/snake.c
+++ Snake Game/snake.c
@@ -1,28 +1,55 @@
+/*Library Includes*/
 #include "snake.h"
 
+/*Global variables*/
 char gSpace = ' ' ;
 char gSnakeSymbol = 'o';
 char gFoodSymbol = '*';
 
+/****************Private function prototypes*************************/
+
+/**
+ * @brief add single uint primary snake
+ */
 static void add_initialSnake(char gameScreen [SCREEN_ROW][SCREEN_COL], snakeUnit* pHead );
+
+/**
+ * @brief add new node at the beginning
+ */
 static snakeUnit* add_nodeAtBegining(snakeUnit* pHead, int newHeadRow, int newHeadCol);
-static void delete_lastNode(snakeUnit* pHead, int* snakeTailRow, int* snakeTailCol);
+
+/**
+ * @brief delete last node
+ */
+static void delete_lastNode(snakeUnit* pHead, int* pSnakeTailRow, int* pSnakeTailCol);
+
+/**
+ * @brief clear the terminal
+ */
+static void clear_terminal(void);
+
+
+
+/********************Library functions ******************************/
 
 
 
 snakeUnit* snake_gameInit(char gameScreen [SCREEN_ROW][SCREEN_COL])
 {   
+    /*Allocate memory for snake head node*/
     snakeUnit *pHead = (snakeUnit *)malloc(sizeof(snakeUnit));
 
+    /*Reset game screen, initialise seed of rand(..) , add initial snake*/
     snake_resetGameScreen(gameScreen);
     srand((unsigned long) time(NULL));
     add_initialSnake(gameScreen, pHead);
 
+    /*Return address of snake head */
     return pHead;
 }
 
 
-void snake_generateFoodPosition(int* const row, int* const col, snakeUnit *pHead)
+void snake_generateFoodPosition(int* const pRow, int* const pCol, snakeUnit *pHead)
 {   
     snakeUnit *ptr;
     int randNum;
@@ -30,20 +57,23 @@ void snake_generateFoodPosition(int* const row, int* const col, snakeUnit *pHead
 
     GENERATE_AGAIN:
 
+    /*Generate random row & column*/
     randNum = rand() % SCREEN_ROW ;
-    *row = randNum;
+    *pRow = randNum;
     randNum = rand() % SCREEN_COL ;
-    *col = randNum;
+    *pCol = randNum;
 
+    /*Check if the generated row & column is overlaping with the snake body or not*/
     ptr = pHead;
     while (ptr != NULL){
-        if (ptr->row == *row && ptr->column == *col){
+        if (ptr->row == *pRow && ptr->column == *pCol){
             generateFlag = 1;
             break;
         }
         ptr = ptr->next;
     }
     
+    /* if overlaping , generate again*/
     if (generateFlag == 1){
         generateFlag = 0;
         goto GENERATE_AGAIN;
@@ -52,9 +82,9 @@ void snake_generateFoodPosition(int* const row, int* const col, snakeUnit *pHead
 
 
 
-
 bool snake_isEatingFood(int foodRow, int foodColumn, snakeUnit* pHead)
-{
+{   
+    /*Check if snake head position & food position are same or not*/
     if (pHead->row == foodRow && pHead->column == foodColumn){
         return True;
     }
@@ -64,9 +94,14 @@ bool snake_isEatingFood(int foodRow, int foodColumn, snakeUnit* pHead)
 
 void snake_increaseSnake(int snakeTailRow, int snakeTailCol, snakeUnit* pHead)
 {   
+    /*Allocate memory for new node*/
     snakeUnit *newUnit = (snakeUnit *)malloc(sizeof(snakeUnit));
     snakeUnit *ptr = pHead;
 
+    /**
+     * Add new node the tail of the snake [linked list] 
+     * position of new tail node will be last deleted tail node [ given by snake_updatePosition(..) ]
+    */
     newUnit->row = snakeTailRow;
     newUnit->column = snakeTailCol;
     newUnit->next = NULL;
@@ -75,20 +110,13 @@ void snake_increaseSnake(int snakeTailRow, int snakeTailCol, snakeUnit* pHead)
         ptr = ptr->next;
     }
     ptr->next = newUnit;
-
 }
 
 
 
-void snake_waitGameScreen(int time)
-{
-    for (volatile long int i = 0; i < (time * 1000); i++);
-}
-
-
-void snake_addFoodToGameScreen(char gameScreen [SCREEN_ROW][SCREEN_COL], const int row, const int col)
+void snake_addFoodToGameScreen(char gameScreen [SCREEN_ROW][SCREEN_COL], const int foodRow, const int foodCol)
 {   
-    gameScreen[row][col] = gFoodSymbol; 
+    gameScreen[foodRow][foodCol] = gFoodSymbol; 
 }
 
 
@@ -98,13 +126,20 @@ void snake_displayGameScreen(snakeUnit* pHead,  char gameScreen [SCREEN_ROW][SCR
     int col = 0;
 
     /*clear screen*/
-    system("clear");
+    clear_terminal();
 
     /*print score*/
-    printf("      SCORE: %d\n", score);
+    printf("SCORE: %d\n", score);
 
-    /*print game board*/
-    printf("----------------------\n");
+    /****print game board****/
+
+    /*Top margin*/
+    for (int temp = 0; temp <= SCREEN_COL+1; temp++){
+        printf("-");
+    }
+    printf("\n");
+
+    /*print matrix*/
     for (row = 0; row < SCREEN_ROW; row++){
         printf("|");
         for (col = 0; col < SCREEN_COL; col++){
@@ -112,10 +147,14 @@ void snake_displayGameScreen(snakeUnit* pHead,  char gameScreen [SCREEN_ROW][SCR
             if (col == (SCREEN_COL - 1))
                 printf("|");
         }
-        if (row != (SCREEN_ROW - 1))
-            printf("\n");
+        printf("\n");
     }
-    printf("\n----------------------\n");
+
+    /*Bottom margin*/
+    for (int temp = 0; temp <= SCREEN_COL+1; temp++){
+        printf("-");
+    }
+    printf("\n");
 }
 
 
@@ -124,7 +163,7 @@ void snake_addSnakeToGameScreen(snakeUnit* pHead, char gameScreen [SCREEN_ROW][S
 {   
     snakeUnit *ptr = pHead;
 
-    /*update game board*/
+    /*add new snake to game screen*/
     while (ptr != NULL){
         gameScreen[ptr->row][ptr->column] = gSnakeSymbol;
         ptr = ptr->next;
@@ -132,27 +171,41 @@ void snake_addSnakeToGameScreen(snakeUnit* pHead, char gameScreen [SCREEN_ROW][S
 }
 
 
-snakeUnit* snake_updateSnakePosition(char userInput, snakeUnit* pHead, int* snakeTailRow, int* snakeTailCol)
+
+snakeUnit* snake_updateSnakePosition(char userInput, snakeUnit* pHead, int* pSnakeTailRow, int* pSnakeTailCol)
 {   
+    /*Store head position before updating snake position*/
     int headRow = pHead->row;
     int headCol = pHead->column;
 
+    /**
+     * snake increasing logic:
+     * step 1. add new node at the beginning [row , column will be according to input]
+     * step 2. delete last node 
+    */
+
     if (userInput == 'w'){
+        /*For inpit 'w', add new node by decresing org. head row by 1 unit [going up]*/
         pHead = add_nodeAtBegining(pHead, (headRow - 1), headCol);
-        delete_lastNode(pHead, snakeTailRow, snakeTailCol);
+        delete_lastNode(pHead, pSnakeTailRow, pSnakeTailCol);
 
     } else if (userInput == 's'){
+        /*For inpit 's', add new node by incresing org. head row by 1 unit [going down]*/
         pHead = add_nodeAtBegining(pHead, (headRow + 1), headCol);
-        delete_lastNode(pHead, snakeTailRow, snakeTailCol);
+        delete_lastNode(pHead, pSnakeTailRow, pSnakeTailCol);
 
     } else if (userInput == 'a'){
+        /*For inpit 'a', add new node by decresing org. head column by 1 unit [going left]*/
         pHead = add_nodeAtBegining(pHead, headRow, (headCol - 1));
-        delete_lastNode(pHead, snakeTailRow, snakeTailCol);
+        delete_lastNode(pHead, pSnakeTailRow, pSnakeTailCol);
 
     } else if (userInput == 'd'){
+        /*For inpit 'd', add new node by decresing org. head column by 1 unit [going right]*/
         pHead = add_nodeAtBegining(pHead, headRow, (headCol + 1));
-        delete_lastNode(pHead, snakeTailRow, snakeTailCol);
+        delete_lastNode(pHead, pSnakeTailRow, pSnakeTailCol);
     }
+
+    /*return updated new head*/
     return pHead;
 }
 
@@ -162,7 +215,7 @@ void snake_resetGameScreen(char gameScreen [SCREEN_ROW][SCREEN_COL])
     int row;
     int col;
 
-    /*reset game board*/
+    /*reset game screen matrix , fill with spaces*/
     for (row = 0; row < SCREEN_ROW; row++){
         for (col = 0; col < SCREEN_COL; col++){
             gameScreen[row][col] = gSpace;
@@ -171,9 +224,13 @@ void snake_resetGameScreen(char gameScreen [SCREEN_ROW][SCREEN_COL])
 }
 
 
-void snake_updateSocreBoard(int *score)
-{
-    *score = *score + 10;
+void snake_updateSocreBoard(int *pScore)
+{   
+    int increment = 0;
+
+    /*score increment increases by 5 after every 500 scores*/
+    increment = 10 + (5 * ((*pScore)/500));
+    *pScore = *pScore + increment;
 }
 
 
@@ -184,6 +241,7 @@ bool snake_isColliding(snakeUnit *pHead)
     int headRow = pHead->row;
     int headCol = pHead->column;
 
+    /*Check if colliding with the (4) walls*/
     if (headRow == -1 || headRow == SCREEN_ROW){
         return True;
 
@@ -191,6 +249,7 @@ bool snake_isColliding(snakeUnit *pHead)
         return True;
     }
 
+    /*Check if colliding with itself*/
     while (ptr != NULL){
         if (ptr->row == headRow && ptr->column == headCol)
             return True;
@@ -200,12 +259,24 @@ bool snake_isColliding(snakeUnit *pHead)
     return Flase;
 }
 
-void snake_terminateGame(char *terminationMsg, int score)
-{
-    system("clear");
-    printf("Your Score: %d\n", score);
-    printf("Game Closed: %s\r\n", terminationMsg);
+
+void snake_terminateGame(char *pTerminationMsg, int score)
+{   
+    /*Print termination messege , score & terminate program*/
+    printf("\n\nYour Score: %d\n", score);
+    printf("Game Over: %s\r\n", pTerminationMsg);
     exit(0);
+}
+
+
+void snake_updateSnakeSpeed(int score, long long int* pUsTimeout)
+{   
+    /* if timeout is 1 ms*/
+    if (*pUsTimeout <= 1000){
+        return;
+    }
+    /*Decrese timeout with increasing score : increases speed*/
+    *pUsTimeout = (*pUsTimeout - score);
 }
 
 
@@ -213,6 +284,7 @@ void snake_terminateGame(char *terminationMsg, int score)
 
 static void add_initialSnake(char gameScreen [SCREEN_ROW][SCREEN_COL], snakeUnit* pHead)
 {   
+    /* add single unit snake to position [0][0]*/
     pHead->row = 0;
     pHead->column = 0;
     pHead->next = NULL;
@@ -222,33 +294,48 @@ static void add_initialSnake(char gameScreen [SCREEN_ROW][SCREEN_COL], snakeUnit
 
 
 static snakeUnit* add_nodeAtBegining(snakeUnit* pHead, int newHeadRow, int newHeadCol)
-{
+{   
+    /*Allocate memory for new node*/
     snakeUnit* temp = (snakeUnit *)malloc(sizeof(snakeUnit));
 
+    /*Add new node at the beginning, update head*/
     temp->row = newHeadRow;
     temp->column = newHeadCol;
     temp->next = pHead;
     pHead = temp;
 
+    /*return new head*/
     return pHead;
 }
 
 
-static void delete_lastNode(snakeUnit* pHead, int* snakeTailRow, int* snakeTailCol)
+static void delete_lastNode(snakeUnit* pHead, int* pSnakeTailRow, int* pSnakeTailCol)
 {
     snakeUnit* ptr1 = pHead;
     snakeUnit* ptr2 = pHead->next;
 
+    /*delete last node*/
     while (ptr2->next != NULL){
         ptr1 = ptr1->next;
         ptr2 = ptr2->next;
     }
     ptr1->next = NULL;
 
-    *snakeTailRow = ptr2->row;
-    *snakeTailCol = ptr2->column;
+    /**
+     * store position (row,col) of deleted node
+     * required for increasing the snake after eating food
+    */
+    *pSnakeTailRow = ptr2->row;
+    *pSnakeTailCol = ptr2->column;
+
+    /*free memory of deleted node*/
     free(ptr2);
 }
 
+
+static void clear_terminal(void)
+{
+    system("clear");
+}
 
 // TODO: 1. find a way to to run snake faster with the increasing score [decrease input timeout]
--- Snake Game/snake.h
+++ Snake Game/snake.h
@@ -1,16 +1,21 @@
+/* Public Headers */
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <unistd.h>
-#include <fcntl.h>
+
+/********Library Definitation***********/
 
 #ifndef SNAKE_H
 #define SNAKE_H
 
-#define SCREEN_ROW  10   // height
-#define SCREEN_COL  20   // width
+/*Constants*/
+#define SCREEN_ROW  20              // board height 
+#define SCREEN_COL  40              // board width  
+#define  DEFAULT_TIMEOUT 500000     // in us (control snake speed)
 
+/* Enum for bool */
 typedef enum {
 
     True  = 1,
@@ -18,6 +23,8 @@ typedef enum {
 
 } bool;
 
+
+/* Structure for storing the position of one single unit of snake*/
 typedef struct snakeUnit {
 
     int row;
@@ -27,18 +34,125 @@ typedef struct snakeUnit {
 } snakeUnit;
 
 
+/************************* Libray API's Prototypes***************************/
+
+/**
+ * @brief  initialise the game 
+ * @param  gameScreen 2D matrix
+ * @return snakeUnit*
+ */
 snakeUnit* snake_gameInit(char gameScreen[SCREEN_ROW][SCREEN_COL]);
-void snake_generateFoodPosition(int* const row, int* const col, snakeUnit *pHead);
-void snake_addFoodToGameScreen(char gameScreen[SCREEN_ROW][SCREEN_COL], const int row, const int col);
-void snake_displayGameScreen(snakeUnit* pHead,  char gameScreen [SCREEN_ROW][SCREEN_COL], int score);
+
+
+/**
+ * @brief  update snake position according to user input , store the last tail position of the snake
+ * @param   userInput       input form user
+ * @param   pHead           pointer to head node of the snake [linked list] 
+ * @param   pSnakeTailRow   pointer for storing last tail position row of the snake
+ * @param   pSnakeTailCol   pointer for storing last tail position col of the snake
+ * @return snakeUnit*
+ */
+snakeUnit* snake_updateSnakePosition(char userInput, snakeUnit* pHead, int* pSnakeTailRow, int* pSnakeTailCol);
+
+
+/**
+ * @brief  check if the snake is eating the food or not  
+ * @param  foodRow row of food position
+ * @param  foodCol column of food position
+ * @param  pHead   pointer to head node of the snake [linked list]
+ * @return bool (True / False )
+ */
 bool snake_isEatingFood(int foodRow, int foodCol, snakeUnit* pHead);
+
+
+/**
+ * @brief  check if the snake is colliding with the walls or with itself 
+ * @param  pHead pointer to head node of the snake [linked list]
+ * @return bool (True / False)
+ */
 bool snake_isColliding(snakeUnit *pHead);
+
+
+/**
+ * @brief  generate new food position
+ * @param  pRow pointer to variable that stores row of generated food position 
+ * @param  pCol pointer to variable that stores column of generated food position 
+ * @param  pHead pointer to head node of the snake [linked list]
+ * @return void 
+ */
+void snake_generateFoodPosition(int* const pRow, int* const pCol, snakeUnit *pHead);
+
+
+/**
+ * @brief  add food to game screen
+ * @param  gameScreen 2D matrix
+ * @param  foodRow row of generated food's position 
+ * @param  foodCol column of generated food's position 
+ * @return void
+ */
+void snake_addFoodToGameScreen(char gameScreen[SCREEN_ROW][SCREEN_COL], const int foodRow, const int foodCol);
+
+/**
+ * @brief  display game screen 
+ * @param  pHead pointer to head node of the snake [linked list]
+ * @param  gameScreen 2D matrix
+ * @param  score last score
+ * @return void
+ */
+void snake_displayGameScreen(snakeUnit* pHead,  char gameScreen [SCREEN_ROW][SCREEN_COL], int score);
+
+
+/**
+ * @brief  display game screen 
+ * @param  snakeTailRow last (deleted) row position of snake tail
+ * @param  snakeTailCol last (deleted) column position of snake tail
+ * @param  pHead pointer to head node of the snake [linked list]
+ * @return void
+ */
 void snake_increaseSnake(int snakeTailRow, int snakeTailCol, snakeUnit* pHead);
-void snake_waitGameScreen(int time);
-snakeUnit* snake_updateSnakePosition(char userInput, snakeUnit* pHead, int* snakeTailRow, int* snakeTailCol);
+
+
+/**
+ * @brief  reset game screen matrix
+ * @param  gameScreen 2D matrix
+ * @return void
+ */
 void snake_resetGameScreen(char gameScreen [SCREEN_ROW][SCREEN_COL]);
+
+
+/**
+ * @brief  add updated sanke to game screen
+ * @param  pHead pointer to head node of the snake [linked list]
+ * @param  gameScreen 2D matrix
+ * @return void
+ */
 void snake_addSnakeToGameScreen(snakeUnit* pHead, char gameScreen [SCREEN_ROW][SCREEN_COL]);
+
+
+/**
+ * @brief  update score board
+ * @param  score last score.
+ * @return void
+ */
 void snake_updateSocreBoard(int *score);
-void snake_terminateGame(char *terminationMsg, int score);
+
+
+/**
+ * @brief terminate game.
+ * @param pTerminationMsg pointer to termination messege to be shown.
+ * @param score last score.
+ * @return void
+ */
+void snake_terminateGame(char *pTerminationMsg, int score);
+
+/**
+ * @brief update snake by decreasing input timeout
+ * @param score last score.
+ * @param pUsTimeout pointer to usTimeout variable
+ * @return void
+ */
+void snake_updateSnakeSpeed(int score, long long int* pUTimeout);
+
+/*******************************************************************************/
 
 #endif
